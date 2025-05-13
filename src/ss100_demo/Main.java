8c97415d03b1422bdc25162fca9157df70a66993
chanhtv235 <chanhtv235@gmail.com>
2025-05-13 08:14:56 +0700

    ss2

---
+++ src/ss100_demo/Main.java
@@ -0,0 +1,47 @@
+package ss100_demo;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        displayMenu();
+    }
+
+    public static void displayMenu() {
+        final int DISPLAY =1;
+        final int ADD =2;
+        final int DELETE =3;
+        final int SEARCH =4;
+        Scanner scanner = new Scanner(System.in);
+        boolean flag = true;
+        while (flag) {
+            System.out.println("-----Chọn chức năng-----\n" +
+                    "1.Hiển thị danh sách\n" +
+                    "2.Thêm mới\n" +
+                    "3.Xoá\n" +
+                    "4.Tìm kiếm\n" +
+                    "5. Thoát");
+            int choose = Integer.parseInt(scanner.nextLine());
+            switch (choose) {
+                case DISPLAY:
+                    System.out.println("Danh sách");
+                    // chức hiển thị
+                    break;
+                case ADD:
+                    System.out.println("Thêm mới");
+                    break;
+                case DELETE:
+                    System.out.println("Xoá");
+                    break;
+                case SEARCH:
+                    System.out.println("Tìm kiếm");
+                    break;
+                default:
+                    flag = false;
+                    break;
+            }
+        }
+
+    }
+}
+
--- src/ss2_array_loop/Demo.java
+++ src/ss2_array_loop/Demo.java
@@ -4,7 +4,36 @@ import java.util.Arrays;
 
 public class Demo {
     public static void main(String[] args) {
-        Double[] numbers = new Integer[2];
-        System.out.println(Arrays.toString((Number[])numbers));
+     // khai báo mảng có  thể lưu dữ liệu kiểu int
+        int[] numbers = {10,20,30};
+        int nums[] = {20,45,56};
+        String[] cars = new String[3]; // mỗi phần tử giá trị null
+        cars[0]= "Huyndai";
+        cars[1]= "Honda";
+        cars[2]= "Toyota";
+
+        System.out.println(Arrays.toString(cars));
+        for (int i = 0; i < cars.length ; i++) {
+           cars[i] = cars[i] + "VN";
+        }
+
+//        for(String car: cars){
+//            car = car +"VN";
+//        }
+
+        System.out.println(Arrays.toString(cars));
+        int[][] matrix1 = {
+                {10,2,4},
+                {34,4,5}
+        };
+        int[][] matrix = new  int[3][3];
+        for (int i = 0; i <matrix.length ; i++) {
+            for (int j = 0; j <matrix[i].length ; j++) {
+                matrix[i][j] = 10;
+            }
+        }
+
+        // duyệt mảng :
+
     }
 }
